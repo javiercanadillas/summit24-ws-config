7a6efade9fb439945caba188a232298eeaeccbba
Javier Cañadillas <javiercm@google.com>
2024-05-21 21:39:40 +0200

    FEAT: better boot scripts mgmt

---
+++ image/scripts/boot/301_add_bootstrap_instructions.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# File:         301_add_bootstrap_instructions.sh
+# Description:  Adds a README.md file with instructions on how to bootstrap the Cloud Workstation
+#
+# Author:       Javier Cañadillas (javier@canadillas.org)
+# Date:         May 2024
+# Version:      1.0
+
+user_name="user"
+group_name="user"
+readme_file="README.md"
+
+check_readme_file() {
+  if [[ ! -f "/root/${readme_file}" ]]; then
+    echo "No README file found in /root, aborting..."
+    exit 1
+  fi
+}
+
+copy_readme_file() {
+  cp -- "/root/${readme_file}" "${HOME}/${readme_file}"
+  chown "${user_name}:${group_name}" "${HOME}/${readme_file}"
+  chmod 644 "${HOME}/${readme_file}"
+}
+
+main() {
+  copy_readme_file
+}
+
+main "$@"
